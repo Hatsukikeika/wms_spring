ad2dcab6980eb092ee1434ef5ac59d22e116b7b4
Hatsukikeika <43552712+Hatsukikeika@users.noreply.github.com>
2021-07-13 04:41:43 -0400

    update account creatation/validation

--- src/main/java/com/wms/service/Impl/AccountServiceImpl.java
+++ src/main/java/com/wms/service/Impl/AccountServiceImpl.java
@@ -1,5 +1,6 @@
 package com.wms.service.Impl;
 
+import java.util.Date;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.wms.DAO.CompMemberRepository;
 import com.wms.DAO.CompanyRepository;
 import com.wms.DAO.FriendPairRepository;
 import com.wms.DAO.UserRepository;
@@ -20,87 +22,95 @@ import com.wms.bean.User;
 import com.wms.bean.Validation;
 import com.wms.bean.enu.GroupType;
 import com.wms.bean.enu.UserRole;
+import com.wms.bean.relations.mtm.CompanyMember;
 import com.wms.bean.relations.mtm.FriendPair;
 import com.wms.model.AccMailContent;
 import com.wms.service.AccountService;
+import com.wms.service.Exceptions.BadAuthParamException;
+import com.wms.service.Exceptions.DataNotFoundException;
 import com.wms.service.Exceptions.FieldMissingException;
+import com.wms.service.Exceptions.ObjectExpiredException;
 import com.wms.service.Exceptions.RegistrationException;
 import com.wms.utils.mailing.exceptions.RegInfoNotFoundException;
-
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
 	@PersistenceContext
 	private EntityManager entityManager;
-	
+
 	@Autowired
 	private CompanyRepository companyRepository;
 	
+	@Autowired
+	private CompMemberRepository compMemberRepository;
+
 	@Autowired
 	private UserRepository userRepository;
 
 	@Autowired
 	private ValidationRepository validationRepository;
-	
+
 	@Autowired
 	private FriendPairRepository friendPairRepository;
-	
+
 	private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
 	private static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern
 			.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
-	//Password length must between 7-35 character.
+	// Password length must between 7-35 character.
 	private static final int MIN_PASSWORD_LENGTH = 7;
 
 	private static final int MAX_PASSWORD_LENGTH = 35;
 
-	//Email length must between 6-35 character.
+	// Email length must between 6-35 character.
 	private static final int MIN_EMAIL_LENGTH = 6;
 
 	private static final int MAX_EMAIL_LENGTH = 35;
-	
+
 	@Override
 	public void createRootAccount(Map<String, Object> signup) {
 		String email = (String) signup.get("email");
 		String password = (String) signup.get("password");
-		
+
 		GroupType gtype = GroupType.get((Integer) signup.get("grouptype"));
 		String invcode = (String) signup.get("invite");
-		
-		checkEmailAndPassword(email,password);
-		
-		if(gtype == GroupType.TYPE_SELLER || gtype == GroupType.TYPE_STORAGE) {
+
+		checkEmailAndPassword(email, password);
+
+		if (gtype == GroupType.TYPE_SELLER || gtype == GroupType.TYPE_STORAGE) {
 			// Parameter is save
 			// create user group
-			Company company = new Company().setType(gtype)
-					.setActivated(false)
-					.setName((String) signup.get("cname"));
+			Company company = new Company().setType(gtype).setActivated(false).setName((String) signup.get("cname"));
 			companyRepository.save(company);
-			
+
 			Company linker = null;
-			if(invcode != null) {
+			if (invcode != null) {
 				linker = companyRepository.findByInvcode(invcode);
-				if(linker == null || linker.getType() == gtype)
+				if (linker == null || linker.getType() == gtype)
 					throw new RegistrationException("invalid invitation code");
-				switch(linker.getType()) {
+				switch (linker.getType()) {
 				case TYPE_SELLER:
-					FriendPair g1 = new FriendPair(linker,company);
+					FriendPair g1 = new FriendPair(linker, company);
 					friendPairRepository.save(g1);
 					break;
 				case TYPE_STORAGE:
-					FriendPair g2 = new FriendPair(company,linker);
+					FriendPair g2 = new FriendPair(company, linker);
 					friendPairRepository.save(g2);
 					break;
 				default:
 				}
 			}
-			
+
 			// create root user using given group
-			User user = new User(company).setActivated(false).setEmail(email).setPassword(bCryptPasswordEncoder.encode(password));
+			User user = new User(company).setActivated(false).setEmail(email)
+					.setPassword(bCryptPasswordEncoder.encode(password));
+
+			CompanyMember cm = new CompanyMember();
+			cm.setCompany(company).setMember(user);
 			
-			switch(gtype) {
+			switch (gtype) {
 			case TYPE_SELLER:
 				user.setRole(UserRole.SELLER);
 				break;
@@ -109,15 +119,19 @@ public class AccountServiceImpl implements AccountService {
 				break;
 			default:
 			}
-			
+
 			userRepository.save(user);
-			
+
 			// create validation info and send the validation email.
 			Validation val = new Validation(user);
+			
 			validationRepository.save(val);
+
+			compMemberRepository.save(cm);
 			
 			sendValidationEmail(email, company.getId(), null, val);
-		}else {
+
+		} else {
 			throw new RegistrationException("invalid group type");
 		}
 
@@ -137,7 +151,26 @@ public class AccountServiceImpl implements AccountService {
 
 	@Override
 	public void validateAccount(Long userid, Long vid, String key) {
-		// TODO Auto-generated method stub
+		Validation validation = validationRepository.findOne(vid);
+		if(validation == null)
+			throw new DataNotFoundException(vid.toString(), "vid", "validation attempt");
+
+		User user = validation.getUser();
+		
+		if(user == null) {
+			validationRepository.delete(validation);
+			throw new RuntimeException("Mapping error");
+		}else if(validation.getExpire() < new Date().getTime()) {
+			validationRepository.delete(validation);
+			throw new ObjectExpiredException("validation");
+		}else if(!validation.getKey().equals(key) || !user.getId().equals(userid)) {
+			throw new BadAuthParamException("invalid validation pair");
+		}else {
+			if(user.getRole() == UserRole.SELLER ||user.getRole() == UserRole.STORAGE)
+				companyRepository.save(user.getOwnedCompany().setActivated(true));
+			userRepository.save(user.setActivated(true));
+			validationRepository.delete(validation);
+		}
 
 	}
 
@@ -161,15 +194,18 @@ public class AccountServiceImpl implements AccountService {
 
 	@Override
 	public User getAccountDetail(Long userid) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		User user = userRepository.findOne(userid);
+		entityManager.detach(user);
+
+		return user.setPassword(null);
 	}
-	
+
 	private void checkEmailAndPassword(String email, String password) {
-		//check if the email or password is missing
+		// check if the email or password is missing
 		if (email == null || password == null)
 			throw new FieldMissingException("email/password");
-		
+
 		// check if the email is duplicated
 		if (userRepository.existsByEmail(email))
 			throw new RegistrationException(email + " is already in use.");
@@ -180,30 +216,29 @@ public class AccountServiceImpl implements AccountService {
 				|| email.length() > MAX_EMAIL_LENGTH)
 			throw new RegistrationException("invalid request format");
 	}
-	
+
 	private void sendValidationEmail(String email, Long compid, String tempass, Validation val) {
 		try {
-			
+
 			String add = "";
 			String subject = "Activate your account";
-			
+
 			Company company = companyRepository.findOne(compid);
-			
-			if(tempass != null) {
+
+			if (tempass != null) {
 				add = "<br/> Temporary password is: " + tempass;
-				subject = String.format("An invitaion from %s %s", company.getOwner().getFirstname(), company.getOwner().getLastname());
+				subject = String.format("An invitaion from %s %s", company.getOwner().getFirstname(),
+						company.getOwner().getLastname());
 			}
-				
-			String vallink = String.format("http://ec2-3-84-110-44.compute-1.amazonaws.com/api/account/val?userid=%s&vid=%s&key=%s",
-					val.getUser().getId(),
-					val.getId(),
-					val.getKey());
-			
-			new AccMailContent()
-					.addReceivers(email)
-					.setSubject(subject)
-					.setTextBody("Please remember your company name to login <br/>" + "Company Name: " + company.getName() + "<br/>"
-							+ "Activation link: " + "<a href=\" " + vallink + " \"> Click Here </a>" + add)
+
+			String vallink = String.format(
+					"http://ec2-3-84-110-44.compute-1.amazonaws.com/api/account/val?userid=%s&vid=%s&key=%s",
+					val.getUser().getId(), val.getId(), val.getKey());
+
+			new AccMailContent().addReceivers(email).setSubject(subject)
+					.setTextBody("Please remember your company name to login <br/>" + "Company Name: "
+							+ company.getName() + "<br/>" + "Activation link: " + "<a href=\" " + vallink
+							+ " \"> Click Here </a>" + add)
 					.send();
 		} catch (RegInfoNotFoundException | MessagingException e) {
 			throw new RegistrationException("Failed to send validation email: " + e.getMessage());
